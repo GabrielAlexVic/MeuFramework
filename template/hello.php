34f69d85662fe503e6366e1af6dd1e731d1ba968
GabrielAlexVic <gabrielvictorio2002@hotmail.com>
2024-05-15 23:12:30 -0300

    front

--- template/hello.php
+++ template/hello.php
@@ -1,5 +1,3 @@
 <?php // framework/hello.php
-require_once __DIR__.'/init.php';
 $input = $request->get('name', 'World');
-$response->setcontent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-$response->send();
+$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
